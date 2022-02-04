88b7b97a4ce5a702391987f4ba9c5a8e697b27d4
Pau Urquizu <pau.urquizu@crono.net>
2022-02-04 13:55:38 +0100

    REvertido Gallery

---
+++ apiculturavalles.com/src/.vuepress/components/BlogPhotos.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-card class="elevation-0" v-for="(photo, i) in photos" :key="i">
+        <v-card-text>
+        <a target="_blank" :href="`/img/${photo}`" ><v-img max-width="400px"  :src="`/img/${photo}`" /></a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    props: ['photos'],
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
--- apiculturavalles.com/src/.vuepress/enhanceApp.js
+++ apiculturavalles.com/src/.vuepress/enhanceApp.js
@@ -1,8 +1,6 @@
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import '@mdi/font/scss/materialdesignicons.scss'
-import Vuex from 'vuex'
-import { GetGithubDiscussions } from "../api/api";
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -12,50 +10,4 @@ export default ({
 }) => {
   Vue.use(Vuetify);
   options.vuetify = new Vuetify({})
-  Vue.use(Vuex);
-
-  const state = {
-    DISCUSSIONS: [],
-    LANG: 'CAT'
-  }
-
-  const getters = {
-    GET_DISCUSSIONS: state => {
-      const discussions = []
-      state.DISCUSSIONS.map(d => d.labels.nodes.map(l => {
-        if(l.name === state.LANG){
-          discussions.push(d)
-        }
-      }));
-      return discussions
-    }
-  }
-
-  const mutations = {
-    SET_DISCUSSIONS(state, discussions){
-      state.DISCUSSIONS = discussions
-    },
-    SET_LANG(state, lang){
-      state.LANG = lang !== "CAT" ? "ES" : lang
-    }
-  }
-
-  const actions = {
-    async API_DISCUSSIONS({commit}){
-      const data = await GetGithubDiscussions()
-      const discussions = data.repository.discussions.nodes
-      commit('SET_DISCUSSIONS',discussions)
-    }
-  }
-
-  const store = new Vuex.Store({
-    state,
-    getters,
-    mutations,
-    actions,
-  })
-  
-  Vue.mixin({store: store})
 }
-
-
